9c62a8632ea06eb3b599eb3f5bf160f61b16f14e
HilmiKamil <hilmihelios@gmail.com>
2026-04-29 14:14:18 +0700

    Tugas 6 Laravel

---
+++ booksales-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function register(Request $request)
+    {
+        // Setup Validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:8'
+        ]);
+        // Check Validator
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //Create User
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        //Cek Keberhasilan
+        if ($user) {
+            return response()->json([
+                'success' => true,
+                'message' => 'User Created Succesfully',
+                'data' => $user
+            ], 201);
+        }
+
+        //Cek Kegagalan
+        return response()->json([
+            'sucess' => false,
+            'message' => 'User Creation Faild'
+        ], 409);
+    }
+
+    public function login(Request $request)
+    {
+        // Setupun Validatior
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Cek Validator
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Get Kredensial dari Request
+        $credentials = $request->only('email', 'password');
+
+        // Cek isFailed
+        if (!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Your Email and Password False'
+            ], 401);
+        }
+
+        // Cek isSuccess
+        return response()->json([
+            'success' => true,
+            'message' => 'Login Successfully',
+            'user' => auth()->guard('api')->user(),
+            'token' => $token,
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Successfully!'
+            ], 200);
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Logout Failed'
+            ], 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- booksales-api/routes/api.php
+++ booksales-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
@@ -10,12 +11,31 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//Auth
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+//============== Public ===============//
+
 //Book
-Route::apiResource('/books', BookController::class);
+Route::apiResource('/books', BookController::class)->only(['index', 'show']);
 
 //Genre
-Route::apiResource('/genres', GenreController::class);
+Route::apiResource('/genres', GenreController::class)->only(['index', 'show']);
 
 //Author
-Route::apiResource('/authors', AuthorController::class);
+Route::apiResource('/authors', AuthorController::class)->only(['index', 'show']);
 
+//============== Only Admin ===============//
+
+Route::middleware(['auth:api'])->group(function () {
+    Route::middleware(['role:admin'])->group(function () {
+        //Book
+        Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
+        //Genre
+        Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
+        //Author
+        Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
+    });
+});
